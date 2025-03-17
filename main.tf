851cd41b0c78097ee49b104b4abb8f429d644973
florintomoiaga24 <florintomoiaga24@gmail.com>
2025-03-17 16:12:53 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,124 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "SSH key pair"
+  default     = "my_ssh_key"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = "${var.key_name}.pem"
+}
+
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+# Subnet public
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-central-1a"
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+# Internet Gateway pentru acces la internet
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "InternetGateway"
+  }
+}
+
+# Route Table pentru acces la internet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Asocierea subnetului public cu Route Table-ul
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "sg_ec2" {
+  vpc_id      = aws_vpc.main_vpc.id
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Instanța EC2 Publică
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+  subnet_id              = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "InstantaJenkins"
+  }
+
+  root_block_device {
+    volume_size = 15
+    volume_type = "gp3"
+  }
+}
+
+# Elastic IP (optional pentru IP fix)
+resource "aws_eip" "eip_public_instance" {
+  instance = aws_instance.public_instance.id
+}
